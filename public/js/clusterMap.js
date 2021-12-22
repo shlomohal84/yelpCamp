a1b40e5c2328c7f13e2dcd6e7436713bc80655ed
shlompi84 <shlomo@LaptopX.localdomain>
2021-12-22 19:52:21 +0200

    reverting initial clusterMap.js coords

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/light-v10',
-	center: [79.24277812359766, 22.990618203108554],
+	center: [-103.5917, 40.6699],
 	zoom: 3
 });
 
